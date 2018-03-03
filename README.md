31877a6eb7bb9d76ddbbd1fd60480b62eb395849
ari <afriwan.phys@gmail.com>
2018-03-03 16:28:14 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # KotlinRecyclerView
+
+[<img src="https://lh3.googleusercontent.com/Dnw-i0Lesv2sblDbdtmml4VQrASPe-CxdZA_UqGrYi5ZZrPW9GS9e5KLFeHiU6MKCA=h900" width="286.583" height="475" alt="Google Play"/>](https://play.google.com/store/apps/details?id=motion.studio.jadwalshalat&hl=in)
--- app/src/main/java/motion/studio/kotlinrecyclerview/MainActivity.kt
+++ app/src/main/java/motion/studio/kotlinrecyclerview/MainActivity.kt
@@ -1,5 +1,9 @@
 package motion.studio.kotlinrecyclerview
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
@@ -14,13 +18,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val adapter: MainRecyclerAdapter = MainRecyclerAdapter(dummyData())
         recyclerview.layoutManager = LinearLayoutManager(this)
         recyclerview.adapter = adapter
         adapter.setOnClickListener {
-            Toast.makeText(this, "Position " + (it+1) + " Clicked" , Toast.LENGTH_SHORT).show();
+            try {
+                toast("Position " + (it+1) + " Clicked")
+                var playstoreuri: Uri = Uri.parse("https://play.google.com/store/apps/details?id=motion.studio.jadwalshalat&hl=in")
+                var playstoreIntent: Intent = Intent(Intent.ACTION_VIEW, playstoreuri)
+                startActivity(playstoreIntent)
+            } catch (exp:Exception) {
+                toast(exp.toString())
+            }
         }
+    }
+
+    fun Activity.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, message, duration).show()
     }
 
     fun dummyData(): ArrayList<String>{
